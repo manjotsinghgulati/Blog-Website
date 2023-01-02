7de6188d5cccdfc496d78e9264727626a15a3330
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-01-02 17:33:18 +0530

    Created Directories & Added Compose Post Functionality

--- app.js
+++ app.js
@@ -8,20 +8,40 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+const posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.render('home', {homeContent: homeStartingContent});
+  console.log(posts);
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {aboutContent: aboutContent});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {contactContent: contactContent});
 });
 
 
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
 
 
-
-
+app.post('/compose', (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect('/');
+});
 
 
 
