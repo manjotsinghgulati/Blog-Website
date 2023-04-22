bd99547354c7d3d1b788497cd843283408c3feac
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-04-22 16:28:58 +0530

    Added Compose Route

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require('lodash');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Welcome to our Daily Journal Blog";
+const aboutContent = "Welcome to Daily Journal Blog, where I document my live and share my experience with the world. I believe that everyone has a story to tell, and I'm excited to share my with you. I believe that there's magic in the mundane, and strive to find it in everything we do. I started this blog as a way to connect with others and share my journey. Whether I'm writing about my travels, my struggles, or my triumphs, I hope that my stories inspire you to live your best life. Thank you for joining me on this adventure. I'm so grateful to have you here!";
+
+const contactContent = "Thank you for visiting my Daily Journal Blog website! I always love to hear from my readers, whether you have a question, a suggestion, or just want to say hello. Please don't hesitate to get in touch. You can email me at manjotsighgulati13@gmail.com . I promise to get back to you as soon as possible.";
 
 const app = express();
 
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const postSchema = new mongoose.Schema({
   title: String,
@@ -75,6 +76,6 @@ app.get('/post/:postId', (req, res) => {
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(4000, function() {
+  console.log("Server started on port 4000");
 });
